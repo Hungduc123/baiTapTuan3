bd86f360d053145f559e4be8d76df9cde3c92776
yunjedi <kingofdragonjacklong2009@gmail.com>
2021-04-12 16:47:21 +0700

    Model

---
+++ app/src/main/java/com/example/baitaptuan1/LoginTableModel.kt
@@ -0,0 +1,22 @@
+package com.example.baitaptuan1
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Model Class
+@Entity(tableName = "Login")
+data class LoginTableModel (
+
+        @ColumnInfo(name = "email")
+        var Email: String,
+
+        @ColumnInfo(name = "password")
+        var Password: String
+
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var Id: Int? = null
+}
